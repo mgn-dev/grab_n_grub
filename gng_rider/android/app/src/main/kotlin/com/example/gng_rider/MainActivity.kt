0b0319c19f9f6001430c2a08ea496ea0e6f662e8
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-09-16 12:22:18 +0200

    Do project creation

---
+++ gng_rider/android/app/src/main/kotlin/com/example/gng_rider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gng_rider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ gng_seller/android/app/src/main/kotlin/com/example/gng_seller/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gng_seller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
